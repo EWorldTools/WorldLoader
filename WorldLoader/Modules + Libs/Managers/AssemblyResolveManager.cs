fa52785813b79519f884fe4a368cd520b8bad399
TheETA <69991448+Hacker1254@users.noreply.github.com>
2022-12-31 02:35:35 -0500

    Notes

--- WorldLoader/Modules + Libs/Managers/AssemblyResolveManager.cs
+++ WorldLoader/Modules + Libs/Managers/AssemblyResolveManager.cs
@@ -17,7 +17,9 @@ namespace WorldLoader.ModulesLibs.Managers
 		internal static List<FileInfo> AdditionalFiles = new();
 		internal static Dictionary<string, Assembly> AdditionalAsmChecks = new();
 
-		public AssemblyResolveManager() {
+		public static Action<Assembly> OnAsmResolve;
+
+		public AssemblyResolveManager() { // TODO : Rewrite this, its done really bad, turn on Debug and you'll see
 			var files = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\WorldLoader\\UnhollowedAsm");
 			if (files == null) throw new Exception("Files Are Null!");
 			foreach (var Asm in files) {
@@ -26,26 +28,53 @@ namespace WorldLoader.ModulesLibs.Managers
 			}
 			AdditionalChecks.Add(Directory.CreateDirectory("UserLibs"));
 			AppDomain.CurrentDomain.AssemblyResolve += (sender, args) => {
+				for (long i = 0; i < AppDomain.CurrentDomain.GetAssemblies().LongCount(); i++)
+					if (args.Name == AppDomain.CurrentDomain.GetAssemblies()[i].FullName) return AppDomain.CurrentDomain.GetAssemblies()[i];
 				string name = args.Name;
 				byte[] ByteData = null;
 				if (args.Name.Contains(','))
 					name = args.Name.Substring(0, args.Name.IndexOf(','));// This isn't too good, but it works ig
-				if (AdditionalFiles.Any(a => a.Name == name)) return Assembly.Load(File.ReadAllBytes(AdditionalFiles.SingleOrDefault(a => a.Name == name).FullName));
-				if (AdditionalAsmChecks.TryGetValue(name, out var prasm)) return prasm;
+				if (AdditionalFiles.Any(a => a.Name == name)) {
+
+					var addFiles = Assembly.Load(File.ReadAllBytes(AdditionalFiles.SingleOrDefault(a => a.Name == name).FullName));
+					invOnRelv(addFiles);
+					return addFiles;
+				}
+				if (AdditionalAsmChecks.TryGetValue(name, out var prasm)) {
+					invOnRelv(prasm);
+					return prasm;
+				}
 				Logs.Debug($"[AssemblyResolve] Failed Finding an Assembly Normally! Checking Unhollowed For Assembly {name}");
-				if (UnhollowedAssemblys.TryGetValue(name + ".dll", out var asm))
-					return Assembly.Load(File.ReadAllBytes(asm));
+				if (UnhollowedAssemblys.TryGetValue(name + ".dll", out var asm)) { 
+					var norAsm = Assembly.Load(File.ReadAllBytes(asm));
+					invOnRelv(norAsm);
+					return norAsm;
+				}
 				if (AdditionalChecks.Count > 0) AdditionalChecks.ForEach(a => {
 					var files = a.GetFiles();
 					var filepath = files.FirstOrDefault(a => a.Name == name).FullName;
 					if (!string.IsNullOrEmpty(filepath))
 						ByteData = File.ReadAllBytes(filepath);
 				});
-				if (ByteData.Length > 10)
-					return Assembly.Load(ByteData);
+				if (ByteData.Length > 10) {
+					var byteDataAssembly = Assembly.Load(ByteData);
+					invOnRelv(byteDataAssembly);
+					return byteDataAssembly;
+				}
 				Logs.Warn($"Sender {args.RequestingAssembly.FullName} Tried to get an Assembly ({name}) and FAILED!"); // I dont think i did this right
 				return null;
 			};
+		}
+
+		private static void invOnRelv(Assembly assembly) {
+			try
+			{
+				OnAsmResolve?.Invoke(assembly);
+			}
+			catch (Exception e)
+			{
+				Logs.Error("Error During OnAsmResolve", e);
+			}
 		}
 
 		/// <summary>
