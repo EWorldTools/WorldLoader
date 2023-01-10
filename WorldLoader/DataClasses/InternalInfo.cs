61fe0b7ac732e0ec2a94821b2639704920cef639
TheETA <69991448+Hacker1254@users.noreply.github.com>
2023-01-09 23:15:10 -0500

    Version Bump

--- WorldLoader/DataClasses/InternalInfo.cs
+++ WorldLoader/DataClasses/InternalInfo.cs
@@ -9,7 +9,7 @@ namespace WorldLoader.DataClasses;
 public class InternalInfo
 {
     public static string Name { get; } = "WorldLoader";
-    public static double Version { get; } = 0.3;
+    public static double Version { get; } = 0.8;
     public static string Description { get; } = "A IL2Cpp Mod Loader, Allowing u to Load C# Managed Dlls";
     public static string Author { get; } = "_1254";
     public static string Thanks { get; } = "Cpp2IL [https://github.com/SamboyCoding/Cpp2IL] is licensed under the MIT License. See LICENSE for the full License.";
